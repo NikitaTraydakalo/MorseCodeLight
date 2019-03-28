7de3a6ea452e4ba7c80f41d3566deb7f3f6f7888
Никита <nikitatraydakalo@macbook-pro-nikita.local>
2019-03-28 18:59:59 +0200

    Initial Commit

---
+++ MorseCodeLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MorseCodeLight
+//
+//  Created by Nikita Traydakalo on 3/28/19.
+//  Copyright © 2019 Nikita Traydakalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
